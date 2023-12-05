cb78b2e50c89f37a9101f23561c24579eea0cec7
Nawa Ferdinand <122266994+Ashi252@users.noreply.github.com>
2023-12-05 21:45:50 +0100

    Update rockPaperScissors.py

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -8,7 +8,7 @@ while True:
     user = input("Type Rock or Paper or Scissors, else exit with 'q': ")
     if user == 'q':
         break
-    if user not in option:
+    if user not in option.lower():
         continue
     randNumber = random.randint(0, 2)
     computerGuess = option[randNumber]
